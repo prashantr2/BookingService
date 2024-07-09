7b445cd5d77d2af3a924844a324e95102310bf67
prashantr2 <prashantrawat2ofc@gmail.com>
2024-07-09 09:27:03 +0530

    Initialized DB in basic skeleton

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
 
 const app = express();
 
@@ -9,8 +11,14 @@ const setupServerAndStart = async() => {
 
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+    
+    app.use('/api', apiRoutes);
 
     app.listen(PORT, () => {
+        if (process.env.SYNC_DB) {
+            db.sequelize.sync({ alter: true });
+        }
+
         console.log(`Server up and running on PORT: ${PORT}`);
     })
 }
