2dffe9fe3646152abe49505b2f6fc900e995b148
prashantr2 <prashantrawat2ofc@gmail.com>
2024-07-12 23:42:37 +0530

    Added basic message queuing with rabbitmq, installed amqplib pkg

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,5 +2,8 @@ require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 3002,
-    FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH
+    FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH,
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+    REMINDER_BINDING_KEY: process.env.REMINDER_BINDING_KEY,
+    MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,8 +3,11 @@ const { BookingController } = require('../../controllers/index')
 
 const router = express.Router();
 
+const bookingController = new BookingController();
+
 // Booking
-router.post('/booking', BookingController.create);
+router.post('/booking', bookingController.create);
+router.post('/publish', bookingController.sendMessageToQueue);
 
 
 module.exports = router;
